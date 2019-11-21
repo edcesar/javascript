87d6bba75a3c6a1ec3aba733cf32d86a095899fa
Ed Cesar <edcesar@edcesar.com>
2019-11-21 10:11:37 -0300

    Form working...

--- src/components/sidebar/sidebar.component.js
+++ src/components/sidebar/sidebar.component.js
@@ -9,10 +9,28 @@ class SidebarComponent {
   setSidebar() {
     document.querySelector(".sidebar-container").innerHTML = `
       <div class="sidebar">
-        <input type="text">
-        <button onClick="$app.user.adicionar()">Criar novo usuario</button>
+        <form id="formUser" onsubmit="return $app.sidebar.onFormUserSubmit(event)">
+          <labe for="name">Name</label>
+          <input type="text" id="name" name="name">
+
+          <labe for="email">Email</label>
+          <input type="text" id="email" name="email">
+
+          <labe for="name">Website</label>
+          <input type="text" id="website" name="website">
+
+          <labe for="name">Fone</label>
+          <input type="text" id="fone" name="fone">
+
+          <button onClick="$app.user.adicionar()">Criar novo usuario</button>
+        </form>
       </div>
     `;
+  }
+
+  onFormUserSubmit(event) {
+    event.preventDefault();
+    return false;
   }
 }
 
